71363e9471840f27d2786835f3fbf7a1eed2e50f
Christopher Robison <cdr@netoasis.net>
2025-08-22 13:41:19 -0700

    Change 8081 to 8080 for all testing ports

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:8081',
+    baseUrl: 'http://localhost:8080',
     viewportWidth: 1280,
     viewportHeight: 720,
     screenshotOnRunFailure: true,
